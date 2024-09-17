ac0f2b6142cabc458cf7141de53fea3d54e310e5
wet_creation <mnedobitkin@gmail.com>
2024-09-17 17:51:02 +0300

    commit

--- deeptr.py
+++ deeptr.py
@@ -3,8 +3,8 @@ from lab3 import deep_trans as dt
 
 text = input("Input text: ")
 
-result = dt.LangDetect(text)
-print(result)
+result = str(dt.LangDetect(text)).split(":")
+print(f"code = {result[0]} confidence = {round(float(result[1]),2)*100}%")
 lang = "ukrainian"
 code_lang = dt.CodeLang(lang)
 print(f"code = {code_lang} lang = {lang}")
